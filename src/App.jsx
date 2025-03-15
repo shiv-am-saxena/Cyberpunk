eb33d129c0d398435b7eac3292bae2af4daaca68
Shivam Saxena <shivam2003official@gmail.com>
2025-03-15 13:42:46 +0530

    Hide video background on smaller screens for improved loading experience

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ export default function App() {
 	return (
 		<>
 			{screenLoading ? <div className={`transition-all ${screenLoading? 'block': 'h-0 duration-500'}`} onClick={()=>{setScreenLoading(false)}}><Loading /> </div>: <div className='h-screen flex flex-col gap-5'>
-				<div className='absolute top-0 left-0 w-full h-full overflow-hidden'>
+				<div className='hidden md:block absolute top-0 left-0 w-full h-full overflow-hidden'>
 					<video autoPlay loop muted className='w-full h-full object-cover'>
 						<source src='/cyberpunk.mp4' type='video/mp4' />
 						Your browser does not support the video tag.
