b7426511ddc87179f22d275009b2497fd2ed636b
Shivam Saxena <shivam2003official@gmail.com>
2025-03-15 13:45:12 +0530

    Add gradient background to main app container for improved aesthetics

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
 	}, []);
 	return (
 		<>
-			{screenLoading ? <div className={`transition-all ${screenLoading? 'block': 'h-0 duration-500'}`} onClick={()=>{setScreenLoading(false)}}><Loading /> </div>: <div className='h-screen flex flex-col gap-5'>
+			{screenLoading ? <div className={`transition-all ${screenLoading? 'block': 'h-0 duration-500'}`} onClick={()=>{setScreenLoading(false)}}><Loading /> </div>: <div className='h-screen flex flex-col gap-5 bg-gradient-to-tr from-yellow-300 to-black overflow-hidden'>
 				<div className='hidden md:block absolute top-0 left-0 w-full h-full overflow-hidden'>
 					<video autoPlay loop muted className='w-full h-full object-cover'>
 						<source src='/cyberpunk.mp4' type='video/mp4' />
