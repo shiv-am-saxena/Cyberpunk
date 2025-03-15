8a41711636f2df523d0014d5c894b977a1362d03
Shivam Saxena <shivam2003official@gmail.com>
2025-03-15 13:22:42 +0530

    Enhance loading screen with video background and update styles

--- src/components/Loader/Loading.jsx
+++ src/components/Loader/Loading.jsx
@@ -1,7 +1,13 @@
 export default function Loading() {
     return (
-        <div className='h-screen w-screen transition-all bg-gradient-to-br to-[#000000] from-[#ffee00bb]'>
-            <h1 className="text-8xl">Loading...</h1>
+        <div className='h-screen w-screen transition-all bg-gradient-to-br to-[#000000] from-[#ffee00bb] loading'>
+            <div className='absolute top-0 left-0 w-full h-full overflow-hidden'>
+					<video autoPlay loop muted className='w-full h-full object-cover'>
+						<source src='/cyberpunk.mp4' type='video/mp4' />
+						Your browser does not support the video tag.
+					</video>
+				</div>
+            <h1 className="text-9xl text-[#338cff] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Loading...</h1>
         </div>
     )
 }
